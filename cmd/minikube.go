bc9debe3f6c753acbc55db5ce7efee05707cd66d
David Gu <sysintelligent@gmail.com>
2021-11-04 07:08:06 -0700

    Add provision minikube

--- cmd/minikube.go
+++ cmd/minikube.go
@@ -2,9 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	memory string = "16384"
+	cpus   string = "4"
 )
 
 // minikubeCmd represents the minikube command
@@ -15,18 +21,27 @@ var minikubeCmd = &cobra.Command{
 }
 
 func startMinikube(cmd *cobra.Command, args []string) error {
-	prg := "minikube"
-	arg1 := "--help"
+	// minikube start --memory=16384 --cpus=4 --kubernetes-version=v1.21.3
+	startCmd := []string{"start",
+		"--memory", memory,
+		"--cpus", cpus,
+	}
 
-	c := exec.Command(prg, arg1)
-	stdout, err := c.Output()
+	fmt.Println("Create Minikube instance ...")
 
+	RunCmd(startCmd)
+	return nil
+}
+
+func RunCmd(arg []string) error {
+	cmd := exec.Command("minikube", arg...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
 	}
-
-	fmt.Print(string(stdout))
 	return nil
 }
 
